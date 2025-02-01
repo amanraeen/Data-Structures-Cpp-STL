0d775db0b6560de4d9e3742250b00a9abe10e794
Mohammad Aman Malik <amankgn108@gmail.com>
2025-02-01 17:58:49 +0530

    reverse array using recursion

---
+++ 01_Recursion/01_Reverse/reverse.cpp
@@ -0,0 +1,29 @@
+// Program for reverse an array using recursion
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void f(int i, int arr[], int n) {
+    if(i>=n/2) return;
+    swap(arr[i], arr[n-i-1]);
+    f(i+1, arr, n);
+}
+
+int main() {
+    int n;
+    cout<<"Enter size of array : ";
+    cin>> n;
+
+    int arr[n];
+
+    cout<<"Enter array : ";
+    for(int i=0; i<n; i++)
+      cin>>arr[i];
+
+    f(0,arr,n);
+
+    for(int i=0; i<n; i++)
+      cout<< arr[i] << " ";
+
+    return 0;  
+}
